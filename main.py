ec1937f7fe13fd4aa5af543cea871c9b2a0736f6
darioerny <dario.erny@stud.edubs.ch>
2022-11-01 11:33:32 +0100

    setup projekt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+    # Use a breakpoint in the gfgfcode line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
