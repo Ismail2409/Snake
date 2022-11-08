830b2eb0a784be9f19c324c7bb51d505dac87f6d
Ismail2409 <ismaildzemaili226@gmail.com>
2022-11-08 11:30:58 +0100

    Code angefangen, schwarze fläche

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 screen = pygame.display.set_mode((400, 500)) # Breite und Höhe
+clock = pygame.time.Clock()
 
 while True:
-    for event in pygame.event.get()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit() # sichergehen dass code geschlossen wird
     pygame.display.update() # in dieser while loop alle unsere Elemente Zeichnen
+    clock.tick(60) # wie schnell die while loop pro Sekunde max rennen kann (max 60 frames pro Sekunde)
