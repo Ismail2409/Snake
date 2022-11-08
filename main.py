e2cf0de82ad6e8cc12a40292bdb80ab46f8c23e1
Ismail2409 <ismaildzemaili226@gmail.com>
2022-11-08 11:09:21 +0100

    erster stand

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import pygame
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-print("Ismail der Bäcker")
-print("moin ismail")
-def print_hi(name):
-    # Use a breakpoint in the gfgfcode line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+pygame.init()
+screen = pygame.display.set_mode((400, 500)) # Breite und Höhe
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    for event in pygame.event.get()
+    pygame.display.update() # in dieser while loop alle unsere Elemente Zeichnen
