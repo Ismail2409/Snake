77d7bbd6d878e5431b252b6d0c99c8157458af42
Ismail2409 <ismaildzemaili226@gmail.com>
2022-11-15 11:28:28 +0100

    grüne fläche und gelernt zu zeichen. Im tutorial gelöscht

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500)) # Breite und Höhe
+screen = pygame.display.set_mode((400, 500)) # Breite und Höhe (display surface) nur eines
 clock = pygame.time.Clock()
 
 while True:
@@ -9,5 +9,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit() # sichergehen dass code geschlossen wird
+    screen.fill((150,200,0)) # Farbe
     pygame.display.update() # in dieser while loop alle unsere Elemente Zeichnen
     clock.tick(60) # wie schnell die while loop pro Sekunde max rennen kann (max 60 frames pro Sekunde)
