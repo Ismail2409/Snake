c2d7cef6d6de2fe59a3cb2831c28b028e4c3fb67
Ismail2409 <ismaildzemaili226@gmail.com>
2022-11-29 11:23:05 +0100

    Code für Schlangengrafik erstellt (Grafik existiert noch nicht)

--- main.py
+++ main.py
@@ -26,6 +26,49 @@ class SNAKE:
         self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
 
     def draw_snake(self):
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index,block in enumerate(self.body): # alle blocke im schlangenkörper schauen auch die davor und hinter
+            x_pos = int(block.x * cell_size) # Positionierung Blöcke
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+
+            if index == 0:
+                screen.blit(self.head_right,block_rect)
+            elif index == len(self.body)
+                screen.blit(self.tail,block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal,block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1: # schauen auf vorherigen und nächsten block und wie sie in relation sind
+                        screen.blit(self.body_tl,block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl,block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr,block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br,block_rect)
+
+    def update_head_graphics(self): # ein vektor vom anderen subtrahieren um relation herausfinden
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1,0): self.head = self.head_left
+        elif head_relation == Vector2(-1,0): self.head = self.head_right
+        elif head_relation == Vector2(0,1): self.head = self.head_up
+        elif head_relation == Vector2(0,-1): self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
+
 
 
 
