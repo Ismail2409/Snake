97dc7c850a20881216990831725cdf73298193bd
Ismail2409 <ismaildzemaili226@gmail.com>
2023-01-13 13:45:08 +0100

    Startbildschirm hinzugefügt und mit dem Game verknüpft.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 import random
 import sys
 from pygame.math import Vector2  # um nicht immer pygame.math zu schreiben
+from button import Button
 
 
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]  # startgrösse
-        self.direction = Vector2(0, 0)
+        self.direction = Vector2(1, 0)
         self.new_block = False
         self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
         self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
@@ -100,12 +101,15 @@ class SNAKE:
     def reset(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(0, 0)
+
+
 class FRUIT:  # Frucht erstellen
     def __init__(self):  # x und y position erstellen
         self.randomize()
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)  # viereck erstellen (x,y,w,h)
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size,
+                                 cell_size)  # viereck erstellen (x,y,w,h)
         screen.blit(apple, fruit_rect)
 
     # pygame.draw.rect(screen,(126,166,114),fruit_rect) # viereck zeichnen (öberfläche, farbe, viereck) # man braucht jetzt rec nicht mehr
@@ -118,7 +122,7 @@ class FRUIT:  # Frucht erstellen
 
 class MAIN:
     def __init__(self):
-        self.snake = SNAKE()  # wenn class main gemacht wird werden auch class snake und class fruit gemacht
+        self.snake = SNAKE()  # wenn class main gemacht wird, werden auch class snake und class fruit gemacht
         self.fruit = FRUIT()
 
     def update(self):
@@ -134,13 +138,14 @@ class MAIN:
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:  # wenn frucht = auf schlangenkopf
-            self.fruit.randomize()  # fruch umpositionieren
+            self.fruit.randomize()  # frucht umpositionieren
             self.snake.add_block()  # block an schlange hinzufügen
             self.snake.play_crunch_sound()  # sobald schlangenkopf stelle mit object berührt = sound
 
-        for block in  self.snake.body[1:]:
+        for block in self.snake.body[1:]:
             if block == self.fruit.pos:
-                self.fruit.randomize()
+                self.fruit.randomize() # damit die Frucht nicht auf dem Körper spawned
+
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:  # schauen ob schlange aus map, 0 ist kopf
             self.game_over()
@@ -171,54 +176,143 @@ class MAIN:
         score_surface = game_font.render(score_text, True, (56, 74, 12))
         score_x = int(cell_size * cell_number - 60)
         score_y = int(cell_size * cell_number - 40)
-        score_rect = score_surface.get_rect(center = (score_x, score_y))
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
         screen.blit(score_surface, score_rect)
-        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))  # damit apfel nebem score angezeigt wird
-        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width, apple_rect.height)  # Zahl zu nah am Rahmen nach score_rect.width eig. + zahl (funktioniert nicht)
+        apple_rect = apple.get_rect(
+            midright=(score_rect.left, score_rect.centery))  # damit apfel nebem score angezeigt wird
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width,
+                              apple_rect.height)  # Zahl zu nah am Rahmen nach score_rect.width eig. + zahl (funktioniert nicht)
 
         pygame.draw.rect(screen, (167, 209, 61), bg_rect)
         screen.blit(score_surface, score_rect)
         screen.blit(apple, apple_rect)
         pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)  # Rahmen
 
-pygame.mixer.pre_init(44100,-16,2,512)
+
+# Startbildschirm
+def get_font(size):  # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def play():
+
+    screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+
+    SCREEN_UPDATE = pygame.USEREVENT
+    pygame.time.set_timer(SCREEN_UPDATE, 150)  # alle 150 millisekunde wird timer ausgelöst speed
+
+    main_game = MAIN()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()  # sichergehen, dass code geschlossen wird
+            if event.type == SCREEN_UPDATE:
+                main_game.update()
+            if event.type == pygame.KEYDOWN:  # trigger für tastatur
+                if event.key == pygame.K_UP:  # pfeil oben
+                    if main_game.snake.direction.y != 1:  # solange nicht nach unten bewegen
+                        main_game.snake.direction = Vector2(0, -1)  # pfeil oben = hoch
+                if event.key == pygame.K_RIGHT:
+                    if main_game.snake.direction.x != -1:
+                        main_game.snake.direction = Vector2(1, 0)
+                if event.key == pygame.K_DOWN:
+                    if main_game.snake.direction.y != -1:
+                        main_game.snake.direction = Vector2(0, 1)
+                if event.key == pygame.K_LEFT:
+                    if main_game.snake.direction.x != 1:
+                        main_game.snake.direction = Vector2(-1, 0)
+
+        screen.fill((150, 200, 0))  # Farbe
+        main_game.draw_elements()
+        pygame.display.update()  # in dieser while loop alle unsere Elemente Zeichnen
+        clock.tick(60)  # wie schnell die while loop pro Sekunde max rennen kann (max 60 frames pro Sekunde)
+
+        #pygame.display.update()
+
+
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
 pygame.init()
-cell_size = 38
+
+cell_size = 37
 cell_number = 19
-screen = pygame.display.set_mode((cell_number * cell_size,
-                                  cell_number * cell_size))  # Breite und Höhe (400x400 pixel) (display surface) gibt nur eins
+
 clock = pygame.time.Clock()
-apple = pygame.image.load('Graphics/apple-min.png').convert_alpha()  # Apfel der in Dateien gespeichert ist soll eigefügt werden
 game_font = pygame.font.Font(None, 30)  # TTF file herunterladen oder None schreiben (30=grösse)
 
-SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 150)  # alle 150 millisek. wird timer ausgelöst
+screen = pygame.display.set_mode((cell_number * cell_size,
+                                  cell_number * cell_size))  # Breite und Höhe (400x400 pixel) (display surface) gibt nur eins
+apple = pygame.image.load(
+    'Graphics/apple-min.png').convert_alpha()  # Apfel der in Dateien gespeichert ist soll eingefügt werden
 
-main_game = MAIN()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()  # sichergehen dass code geschlossen wird
-        if event.type == SCREEN_UPDATE:
-            main_game.update()
-        if event.type == pygame.KEYDOWN:  # triger für tastatur
-            if event.key == pygame.K_UP:  # pfeil oben
-                if main_game.snake.direction.y != 1:  # solange nicht nach unten beewegen
-                    main_game.snake.direction = Vector2(0, -1)  # pfeil oben = hoch
-            if event.key == pygame.K_RIGHT:
-                if main_game.snake.direction.x != -1:
-                    main_game.snake.direction = Vector2(1, 0)
-            if event.key == pygame.K_DOWN:
-                if main_game.snake.direction.y != -1:
-                    main_game.snake.direction = Vector2(0, 1)
-            if event.key == pygame.K_LEFT:
-                if main_game.snake.direction.x != 1:
-                    main_game.snake.direction = Vector2(-1, 0)
+SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    screen.fill((150, 200, 0))  # Farbe
-    main_game.draw_elements()
-    pygame.display.update()  # in dieser while loop alle unsere Elemente Zeichnen
-    clock.tick(60)  # wie schnell die while loop pro Sekunde max rennen kann (max 60 frames pro Sekunde)
+pygame.display.set_caption("Menu")
 
+BG = pygame.image.load("assets/Background.png")
+main_menu()
